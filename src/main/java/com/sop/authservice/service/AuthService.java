968625a257e2566f061f1ac2f8b5cc45efefbeb3
it63070029 <63070029@it.kmitl.ac.th>
2022-11-22 00:33:39 +0700

    Initial commit

---
+++ src/main/java/com/sop/authservice/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.sop.authservice.service;
+
+import com.sop.authservice.entities.AuthRequest;
+import com.sop.authservice.entities.AuthResponse;
+import com.sop.authservice.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final JwtUtil jwt;
+
+    @Autowired
+    public AuthService(final JwtUtil jwt){
+        this.jwt = jwt;
+    }
+
+    public AuthResponse logIn(AuthRequest authRequest){
+        // check email and pasword here
+        User user = User.builder()
+                .id("1")
+                .email(authRequest.getEmail())
+                .pasword(authRequest.getPassword())
+                .role("admin")
+                .build();
+        String accessToken = jwt.generate(user, "ACCESS");
+        String refreshToken = jwt.generate(user, "REFRESH");
+
+        return new AuthResponse(accessToken, refreshToken);
+    }
+}
